d8e012a79adde22b3f6040302a689e8edfa4dfcb
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-27 12:26:55 -0500

    Update: Add script monitor detected

--- .config/scripts_jakepys/monitors/toggle_screens.sh
+++ .config/scripts_jakepys/monitors/toggle_screens.sh
@@ -5,26 +5,8 @@
 
 if xrandr | grep "HDMI-1 connected"; then
     # Si el HDMI está conectado
-    xrandr --output HDMI-1 --auto --primary --output eDP-1 --off
+    ~/.screenlayout/hdmi-1.sh
 else
     # Si mi portatil esta abierto
     xrandr --output eDP-1 --auto --primary --output HDMI-1 --off
 fi
-
-# Los anoto porque soy malo recordando xd
-
-# Crear /etc/systemd/logind.conf
-# Add to logind.conf "HandleLidSwitch=ignore"
-# Crear /etc/systemd/system/toggle-screens.service
-# ADD
-# [Unit]
-#Description=Toggle Screens
-
-#[Service]
-#ExecStart=/home/jakepy/toggle_screens.sh
-
-#[Install]
-#WantedBy=multi-user.target
-
-# sudo systemctl enable toggle-screens.service
-
