4c735a7e4eabc8deda26af9a43f740c25ad74756
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-12-03 19:09:33 -0500

    Update: GGG

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "bashls",
+                    "clangd", 
+                    "cmake", 
+                    "cssls", 
+                    "html",
+                    "nimls",
+                    "nim_langserver",
+                    "jedi_language_server",
+                    "pyright",
+                    "pylsp",
+                    "denols",
+                    "ts_ls"
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        end
+    }
+}
+
